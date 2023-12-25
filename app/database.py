73df585cfe614dacd560d6288953f9518e20db62
AstroDev <ferrari.codefire@hotmail.com>
2023-12-25 12:00:58 -0500

    Database configuration

--- app/database.py
+++ app/database.py
@@ -2,10 +2,8 @@ import sqlite3
 
 def create_connection(database_name='database.db'):
     
-    connection = sqlite3.connect('db/database.db')
+    connection = sqlite3.connect('../db/database.db')
     return connection
-
-
 
 def create_table(connection):
     """
@@ -27,3 +25,57 @@ def create_table(connection):
         )
     ''')
     connection.commit()
+
+    # Table schema output (debug)
+    cursor.execute("PRAGMA table_info(users)")
+    print(cursor.fetchall())
+
+def insert_user(connection, name, email):
+    """
+    Insert a new user into 'users' table.
+
+    Args:
+    
+    connection(sqlite3.Connection): Connection object.
+    name (str): The name of the user.
+    age (int): The age of the user.
+
+    """
+
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO users (name, email) VALUES (?, ?)', (name, email))
+    connection.commit()
+
+def get_users(connection):
+    """
+    Retrieve all users from the 'users' table.
+
+    Args:
+        connection (sqlite3.Connection): The database connection object.
+
+    Returns:
+        list: A list of tuples representing users.
+    """
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM users')
+    return cursor.fetchall()
+
+def main():
+
+    database_name = 'database.db'
+    connection = create_connection(database_name)
+    create_table(connection)
+
+    insert_user(connection, 'John Doe', "EMAIL")
+    insert_user(connection, 'Jane Smith', "EMAIL")
+
+    # Retrieve and print all users
+    users = get_users(connection)
+    for user in users:
+        print(user)
+
+    # Close the connection
+    connection.close()
+
+if __name__ == "__main__":
+    main()
